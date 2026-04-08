9dcf74ad4c413cced4e28615b53d64cb9c1cdd90
Fernando <fernandorequenaserra@gmail.com>
2026-04-08 12:17:18 +0200

    add workflows folder to ci cd tests 3

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,5 +1,4 @@
-import { describe, expect, test, vi } from 'vitest'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { describe, expect, test } from 'vitest'
 import { getGifsByQuery } from './get-gifs-by-query.action'
 
 describe('getGifsByQuery', () => {
